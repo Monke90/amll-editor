afc5d9f67b218de1913e9d3bed4c8c18b41a03bd
Monke90 <108591362+Monke90@users.noreply.github.com>
2026-08-13 16:49:12 -0400

    Add language attribute to Syllable.vue input

--- src/ui/editor/content/Syllable.vue
+++ src/ui/editor/content/Syllable.vue
@@ -41,6 +41,7 @@
         v-model="inputModel"
         size="large"
         :spellcheck="prefStore.spellCheckEnabled"
+        lang="en"
         @keydown="handleKeydown"
         @focus="handleFocus"
         @compositionend="hijackCompositionBackquote"
